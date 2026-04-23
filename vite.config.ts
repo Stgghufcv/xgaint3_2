fdcee5f415dec2133ac49f07f164195f3269d70e
FluffyBunny <wangyihao98@gmail.com>
2026-04-23 13:52:58 +0800

    404 bug fix

--- vite.config.ts
+++ vite.config.ts
@@ -3,9 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/xgaint3_2/',
-  server: {
-    port: 5173,
-    open: true,
-  },
+  base: '/xgaint3_2/', // 确保这里和你的仓库名一模一样，前后都有斜杠
 })
